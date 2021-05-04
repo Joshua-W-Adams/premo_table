562878d06c0979d96cb316b8396fd885765af80b
Josh Adams <joshua.william.adams@gmail.com>
2021-05-04 12:51:27 +0800

    refractor example

--- example/lib/app/premo_table_builder/premo_table_builder.dart
+++ example/lib/app/premo_table_builder/premo_table_builder.dart
@@ -1,135 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:premo_table/premo_table.dart';
-import 'package:example/models/sample_data_model.dart';
-import 'package:example/services/mock_data_service.dart';
 
-class PremoTableBuilder extends StatefulWidget {
-  final MockDataService mockDataService;
+class PremoTableBuilder<T extends IUniqueIdentifier> extends StatelessWidget {
+  final TableBloc<T> tableBloc;
 
   PremoTableBuilder({
-    required this.mockDataService,
+    required this.tableBloc,
   });
-
-  @override
-  _PremoTableBuilderState createState() => _PremoTableBuilderState();
-}
-
-class _PremoTableBuilderState extends State<PremoTableBuilder> {
-  TableBloc<SampleDataModel>? _tableBloc;
-
-  @override
-  void initState() {
-    super.initState();
-
-    /// create BLoC
-    _tableBloc = TableBloc(
-      inputStream: widget.mockDataService.stream,
-      columnNames: ['Id', 'Name', 'Age', 'Enabled', 'DOB', 'City', 'Salary'],
-      cellValueBuilder: (rowModel, columnIndex) {
-        /// rowModels can be null if the location being generated in the user
-        /// interface is a deleted row.
-        if (columnIndex == 0) {
-          return rowModel?.id;
-        } else if (columnIndex == 1) {
-          return rowModel?.name;
-        } else if (columnIndex == 2) {
-          return rowModel?.age?.toString();
-        } else if (columnIndex == 3) {
-          return rowModel?.enabled.toString();
-        } else if (columnIndex == 4) {
-          return rowModel?.dateOfBirth.toString();
-        } else if (columnIndex == 5) {
-          return rowModel?.city;
-        } else if (columnIndex == 6) {
-          return rowModel?.salary.toString();
-        }
-      },
-
-      /// sort to applied to all input data before it is released on the table
-      /// bloc stream
-      defaultSortCompare: (a, b) {
-        return a.id.compareTo(b.id);
-      },
-
-      /// sort function to run on each column
-      sortCompare: (col, asc, a, b) {
-        /// invert objects for asc and desc as applicable
-        if (asc != true) {
-          final SampleDataModel c = a;
-          a = b;
-          b = c;
-        }
-
-        /// convert objects to Maps. Enabling referencing of map positions.
-        List aList = a.toMap().values.toList();
-        List bList = b.toMap().values.toList();
-
-        if (col == 3) {
-          int valA = aList[col] == true ? 1 : 0;
-          int valB = bList[col] == true ? 1 : 0;
-          return valA.compareTo(valB);
-        } else {
-          /// comparator functon returns -1 = less, 0 = equal, 1 = greater than
-          return aList[col].compareTo(bList[col]);
-        }
-      },
-
-      /// filter function to run on each column
-      onFilter: (rowModel, col, value) {
-        if (value != '') {
-          /// test less than filter
-          // if (col == 0) {
-          //   int id = int.parse(rowModel.id);
-          //   int filterValue = int.parse(value);
-          //   return id < filterValue;
-          // }
-
-          /// convert objects to Maps. Enabling referencing of map positions.
-          List itemList = rowModel.toMap().values.toList();
-
-          return itemList[col]!.toString().contains(value);
-        }
-
-        return true;
-      },
-
-      // onUpdate: (item, col, value) async {
-      //   /// store item details in model instance
-      //   if (col == 0) {
-      //     /// N/A - non editable column
-      //   } else if (col == 1) {
-      //     item.name = value;
-      //   } else if (col == 2) {
-      //     item.age = num.tryParse(value);
-      //   } else if (col == 3) {
-      //     item.enabled = (value.toLowerCase() == 'true');
-      //   } else if (col == 4) {
-      //     item.dateOfBirth = DateTime.parse(value);
-      //   } else if (col == 5) {
-      //     item.city = value;
-      //   }
-      //   return Future.delayed(Duration(milliseconds: 1000));
-      // },
-    );
-  }
-
-  @override
-  void dispose() {
-    _tableBloc!.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     /// properties to standardise colours across table instance
     ThemeData _theme = Theme.of(context);
-
-    /// interference with gesture detector in cell.
-    // GestureDetector(
-    //   onTap: () {
-    //     _tableBloc.clearSelection();
-    //     _tableBloc.clearHover();
-    //   },
 
     /// generate instance of table
     return Column(
@@ -143,8 +25,8 @@ class _PremoTableBuilderState extends State<PremoTableBuilder> {
         ),
         SizedBox(height: 16.0),
         Expanded(
-          child: PremoTable<SampleDataModel>(
-            tableBloc: _tableBloc!,
+          child: PremoTable<T>(
+            tableBloc: tableBloc,
             columnBackgroundColor: _theme.primaryColor,
             columnTextStyle: _theme.primaryTextTheme.bodyText1,
             columnWidthBuilder: (col) {
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:example/app/sample_data_table/sample_data_table.dart';
+import 'package:example/models/sample_data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:premo_table/premo_table.dart';
 import 'package:example/app/premo_table_builder/premo_table_builder.dart';
@@ -11,47 +12,169 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /// generate data service that provide stream of mocked server data
-    MockDataService mockDataService = MockDataService();
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Column(
-          children: [
-            TestCases(
-              mockDataService: mockDataService,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 2,
-                itemBuilder: (context, index) {
-                  Widget child;
-                  if (index == 0) {
-                    child = PremoTableBuilder(
-                      mockDataService: mockDataService,
-                    );
-                  } else if (index == 1) {
-                    child = SampleDataTable();
-                  } else {
-                    child = Container();
-                  }
-                  return Container(
-                    alignment: Alignment.center,
-                    height: 500,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: child,
-                    ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  /// generate data service that provide stream of mocked server data
+  MockDataService mockDataService = MockDataService();
+
+  /// BloC for sample tables to be generated
+  TableBloc<SampleDataModel>? _tableBloc;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// create BLoC
+    _tableBloc = TableBloc(
+      inputStream: mockDataService.stream,
+      columnNames: ['Id', 'Name', 'Age', 'Enabled', 'DOB', 'City', 'Salary'],
+      cellValueBuilder: (rowModel, columnIndex) {
+        /// rowModels can be null if the location being generated in the user
+        /// interface is a deleted row.
+        if (columnIndex == 0) {
+          return rowModel?.id;
+        } else if (columnIndex == 1) {
+          return rowModel?.name;
+        } else if (columnIndex == 2) {
+          return rowModel?.age?.toString();
+        } else if (columnIndex == 3) {
+          return rowModel?.enabled.toString();
+        } else if (columnIndex == 4) {
+          return rowModel?.dateOfBirth.toString();
+        } else if (columnIndex == 5) {
+          return rowModel?.city;
+        } else if (columnIndex == 6) {
+          return rowModel?.salary.toString();
+        }
+      },
+
+      /// sort to applied to all input data before it is released on the table
+      /// bloc stream
+      defaultSortCompare: (a, b) {
+        return a.id.compareTo(b.id);
+      },
+
+      /// sort function to run on each column
+      sortCompare: (col, asc, a, b) {
+        /// invert objects for asc and desc as applicable
+        if (asc != true) {
+          final SampleDataModel c = a;
+          a = b;
+          b = c;
+        }
+
+        /// convert objects to Maps. Enabling referencing of map positions.
+        List aList = a.toMap().values.toList();
+        List bList = b.toMap().values.toList();
+
+        if (col == 3) {
+          int valA = aList[col] == true ? 1 : 0;
+          int valB = bList[col] == true ? 1 : 0;
+          return valA.compareTo(valB);
+        } else {
+          /// comparator functon returns -1 = less, 0 = equal, 1 = greater than
+          return aList[col].compareTo(bList[col]);
+        }
+      },
+
+      /// filter function to run on each column
+      onFilter: (rowModel, col, value) {
+        if (value != '') {
+          /// test less than filter
+          // if (col == 0) {
+          //   int id = int.parse(rowModel.id);
+          //   int filterValue = int.parse(value);
+          //   return id < filterValue;
+          // }
+
+          /// convert objects to Maps. Enabling referencing of map positions.
+          List itemList = rowModel.toMap().values.toList();
+
+          return itemList[col]!.toString().contains(value);
+        }
+
+        return true;
+      },
+
+      // onUpdate: (item, col, value) async {
+      //   /// store item details in model instance
+      //   if (col == 0) {
+      //     /// N/A - non editable column
+      //   } else if (col == 1) {
+      //     item.name = value;
+      //   } else if (col == 2) {
+      //     item.age = num.tryParse(value);
+      //   } else if (col == 3) {
+      //     item.enabled = (value.toLowerCase() == 'true');
+      //   } else if (col == 4) {
+      //     item.dateOfBirth = DateTime.parse(value);
+      //   } else if (col == 5) {
+      //     item.city = value;
+      //   }
+      //   return Future.delayed(Duration(milliseconds: 1000));
+      // },
+    );
+  }
+
+  @override
+  void dispose() {
+    _tableBloc!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _tableBloc!.deselect();
+      },
+      child: Column(
+        children: [
+          TestCases(
+            mockDataService: mockDataService,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                Widget child;
+                if (index == 0) {
+                  child = PremoTableBuilder<SampleDataModel>(
+                    tableBloc: _tableBloc!,
                   );
-                },
-              ),
+                } else if (index == 1) {
+                  child = SampleDataTable();
+                } else {
+                  child = Container();
+                }
+                return Container(
+                  alignment: Alignment.center,
+                  height: 500,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: child,
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
